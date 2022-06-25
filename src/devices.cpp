fdc9aadbf0374154ee550a6c8c1d82e9d9d81d3b
Oscar McCullough <odm3productions@gmail.com>
2022-06-25 19:15:37 -0400

    Update motors

--- src/devices.cpp
+++ src/devices.cpp
@@ -1,11 +1,11 @@
 #include "devices.h"
 
-Motor leftDrive1(1, false);
-Motor leftDrive2(2, false);
-Motor leftDrive3(3, false);
+Motor leftDrive1(14, false);
+Motor leftDrive2(17, false);
+Motor leftDrive3(18, false);
 Motor rightDrive1(4, true);
 Motor rightDrive2(5, true);
-Motor rightDrive3(6, true);
+Motor rightDrive3(3, true);
 Motor intake(7, E_MOTOR_GEARSET_06, false, E_MOTOR_ENCODER_DEGREES);
 Motor flywheel(8, E_MOTOR_GEARSET_06, false, E_MOTOR_ENCODER_DEGREES);
 Controller driverController(CONTROLLER_MASTER);
